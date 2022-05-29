1483965c2822046f2e811a9efa81b7ba742e4b60
Hugo Castro de Deco <hugodeco@sufficit.com.br>
2022-05-29 20:16:35 -0300

    agiscript attribute

---
+++ src/FastAGI/AGIScriptAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sufficit.Asterisk.FastAGI
+{
+    /// <summary>
+    /// Indicates that is an AGI Script with a valid route 
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class)]
+    public class AGIScriptAttribute : Attribute
+    {
+        public AGIScriptAttribute(string route)
+        {
+            if (string.IsNullOrWhiteSpace(route))
+                throw new ArgumentNullException("route");
+
+            Route = route;
+        }
+
+        /// <summary>
+        /// Route to access this script
+        /// </summary>
+        public string Route { get; }
+
+        /// <summary>
+        /// Load and listen this script at startup
+        /// </summary>
+        public bool StartUp { get; set; }
+    }
+}
