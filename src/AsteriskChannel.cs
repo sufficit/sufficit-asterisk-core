353101c963566fc1804cd06948375b45cc983f53
Hugo Castro de Deco <hugodeco@sufficit.com.br>
2022-10-10 12:16:57 -0300

    nullable;

--- src/AsteriskChannel.cs
+++ src/AsteriskChannel.cs
@@ -20,38 +20,48 @@ namespace Sufficit.Asterisk
 
         public AsteriskChannel(string source)
         {
-            if (!string.IsNullOrWhiteSpace(source))
+            if (string.IsNullOrWhiteSpace(source))
+                throw new ArgumentNullException("source");
+            
+            Id = source;
+            if (Id.Contains("/"))
             {
-                Id = source;
-                if (Id.Contains("/"))
-                {
-                    var splitted = Id.Split('/');
-                    var tech = splitted[0];
-                    Protocol = tech.ToAsteriskChannelProtocol();
+                var splitted = Id.Split('/');
+                var tech = splitted[0];
+                Protocol = tech.ToAsteriskChannelProtocol();
 
-                    var track = splitted[1];
-                    var separator = track.LastIndexOf('-');
-                    if (separator > -1)
-                    {
-                        Suffix = track.Substring(separator + 1, track.Length - (separator + 1));
-                        Name = track.Substring(0, separator);
-                    }
-                    else
-                    {
-                        Name = track;
-                    }
-                } 
-            }
-            else { throw new ArgumentNullException("source"); }
+                var track = splitted[1];   
+                var separator = track.LastIndexOf('-');
+                if (separator > -1)
+                {
+                    Suffix = track.Substring(separator + 1, track.Length - (separator + 1));
+                    Name = track.Substring(0, separator);
+                }
+                else
+                {
+                    Name = track;
+                }
+
+                if (Name.Contains("@"))
+                {
+                    var withContext = Name.Split('@');
+                    if (withContext.Length >= 1)
+                        Context = withContext[1];
+
+                    Name = withContext[0];
+                }
+            }            
         }
 
-        public string Id { get; set; }
+        public string Id { get; set; } = default!;
 
         public AsteriskChannelProtocol Protocol { get; set; }
 
-        public string Name { get; set; }
+        public string? Name { get; set; }
 
-        public string Suffix { get; set; }
+        public string? Context { get; set; }
+
+        public string? Suffix { get; set; }
 
         public string GetPeer() => $"{Protocol}/{Name}";
 
