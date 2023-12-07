73b245c3b10f340ff8787856766ce000d3f90551
Hugo Castro de Deco <hugodeco@sufficit.com.br>
2023-12-07 09:35:29 -0300

    iaccountcode

---
+++ src/Manager/Events/Interfaces/IAccountCode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sufficit.Asterisk.Manager.Events
+{
+    /// <summary>
+    /// Used by events with accountcode property, useful to filter events
+    /// </summary>
+    public interface IAccountCode
+    {
+        string? AccountCode { get; }
+    }
+}
