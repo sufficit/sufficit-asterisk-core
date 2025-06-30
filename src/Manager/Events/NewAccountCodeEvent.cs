1758405fb4a561dcb953a95e2e3847a69c932279
Hugo Castro de Deco <hugodeco@sufficit.com.br>
2025-06-30 16:20:28 -0300

    Atualiza NewAccountCodeEvent para suporte a JSON
    Inclui a diretiva `using System.Text.Json.Serialization;` e altera a herança da classe para `AbstractChannelInfoEvent`. A propriedade `OldAccountCode` agora possui atributos de serialização que permitem ignorá-la se nula ou padrão, enquanto `AccountCode` permanece inalterada.

--- src/Manager/Events/NewAccountCodeEvent.cs
+++ src/Manager/Events/NewAccountCodeEvent.cs
@@ -1,11 +1,17 @@
 using Sufficit.Asterisk.Manager.Events.Abstracts;
+using System.Text.Json.Serialization;
 
 namespace Sufficit.Asterisk.Manager.Events
 {
-    public class NewAccountCodeEvent : ManagerEvent
+    /// <summary>
+    /// Represents an event triggered when a new account code is assigned to a channel.
+    /// </summary>
+    /// <remarks>This event provides information about the new account code and, optionally, the previous
+    /// account code. It is typically used to track changes in account codes for a specific channel.</remarks>
+    public class NewAccountCodeEvent : AbstractChannelInfoEvent
     {
-        public string AccountCode { get; set; }
-
-        public string OldAccountCode { get; set; }
+        [JsonPropertyName("oldaccountcode")]
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull | JsonIgnoreCondition.WhenWritingDefault)]
+        public string? OldAccountCode { get; set; }
     }
 }
